8b08a8ae999f353ac2b7c97cb6a1e17d59c73036
freeok <51998152+freeok@users.noreply.github.com>
2023-11-16 18:56:06 +0800

    Add constants.py
    :bug: fix: 修复领取邮箱奖励问题

--- src/reward.py
+++ src/reward.py
@@ -3,10 +3,11 @@ import time
 
 import pyautogui
 
-from src.pyautogui_utils import PyautoguiUtils
+from src.common.constants import Consts
+from src.util.pyautogui_utils import PAGUtils
 
 # 在 class 中运行的路径
-img_daily_path = 'assets/img/daily/'
+img_daily_path = Consts.IMG_DAILY_PATH
 
 
 # 不在 class 中运行的路径
@@ -20,22 +21,23 @@ class Reward:
 
     # 一键领取任务奖励
     def task_reward(self):
-        PyautoguiUtils.click_gui2(img_daily_path + 'task1.png', img_daily_path + 'task2.png')
-        PyautoguiUtils.click_gui(img_daily_path + 'collect_all.png')
+        PAGUtils.click_gui2(img_daily_path + 'task1.png', img_daily_path + 'task2.png')
+        PAGUtils.click_gui(img_daily_path + 'collect_all.png')
         time.sleep(1)
         pyautogui.click(100, 100)
-        PyautoguiUtils.click_gui(img_daily_path + 'back.png')
+        PAGUtils.click_gui(Consts.IMG_BACK)
 
     # 一键领取邮件奖励
     def mail_reward(self):
-        PyautoguiUtils.click_gui2(img_daily_path + 'mail1.png', img_daily_path + 'mail2.png')
-        PyautoguiUtils.click_gui(img_daily_path + 'collect_all.png')
+        PAGUtils.click_gui2(img_daily_path + 'mail1.png', img_daily_path + 'mail2.png')
+        PAGUtils.click_gui(img_daily_path + 'collect_all.png')
         time.sleep(1)
-        pyautogui.click(100, 100)
+        PAGUtils.click_gui(Consts.IMG_BACK)
+        PAGUtils.click_gui(Consts.IMG_BACK)
 
     # 赠送/领取友情点
     def friendship_reward(self):
-        PyautoguiUtils.click_gui(img_daily_path + 'friends.png')
-        PyautoguiUtils.click_gui(img_daily_path + 'friends_receive.png')
-        PyautoguiUtils.click_gui(img_daily_path + 'friends_give.png')
-        PyautoguiUtils.click_gui(img_daily_path + 'back.png')
+        PAGUtils.click_gui(img_daily_path + 'friends.png')
+        PAGUtils.click_gui(img_daily_path + 'friends_receive.png')
+        PAGUtils.click_gui(img_daily_path + 'friends_give.png')
+        PAGUtils.click_gui(Consts.IMG_BACK)
--- src/secret_realm.py
+++ src/secret_realm.py
@@ -1,10 +1,11 @@
 import time
 
-from src.pyautogui_utils import PyautoguiUtils
+from src.common.constants import Consts
 from src.reward import Reward
+from src.util.pyautogui_utils import PAGUtils
 
-img_dungeon_path = 'assets/img/dungeon/'
-img_daily_path = 'assets/img/daily/'
+img_dungeon_path = Consts.IMG_DUNGEON_PATH
+img_daily_path = Consts.IMG_DAILY_PATH
 
 
 # 秘境
@@ -16,29 +17,29 @@ class SecretRealm:
     # img 图片 n 扫荡次数
     def fun(self, img_list, n):
         if len(img_list) == 1:
-            PyautoguiUtils.click_gui(img_daily_path + img_list[0])
+            PAGUtils.click_gui(img_daily_path + img_list[0])
         elif len(img_list) > 1:
-            PyautoguiUtils.click_gui2(img_daily_path + img_list[0], img_daily_path + img_list[1])
+            PAGUtils.click_gui2(img_daily_path + img_list[0], img_daily_path + img_list[1])
         time.sleep(2)
         # 点击女神球
-        PyautoguiUtils.click_godless_ball()
+        PAGUtils.click_godless_ball()
         # 点击扫荡，按钮默认为蓝色，消耗通行证时为黄色
-        PyautoguiUtils.click_gui2(img_daily_path + 'sweep1.png', img_daily_path + 'sweep2.png')
+        PAGUtils.click_gui2(img_daily_path + 'sweep1.png', img_daily_path + 'sweep2.png')
         # 点击确定
-        PyautoguiUtils.click_gui(img_daily_path + 'confirm.png')
+        PAGUtils.click_gui(img_daily_path + 'confirm.png')
         # 再次扫荡
         for _ in range(n - 1):
-            PyautoguiUtils.click_gui(img_daily_path + 'sweep_again.png')
+            PAGUtils.click_gui(img_daily_path + 'sweep_again.png')
         # 取消
-        PyautoguiUtils.click_gui(img_daily_path + 'cancel.png')
-        PyautoguiUtils.click_gui(img_daily_path + 'back.png')
+        PAGUtils.click_gui(img_daily_path + 'cancel.png')
+        PAGUtils.click_gui(img_daily_path + 'back.png')
 
     # 扫荡秘境
     def sweep(self):
         print('==> 开始扫荡秘境')
 
         # 进入秘境
-        PyautoguiUtils.click_gui2(img_daily_path + 'secret_realm1.png', img_daily_path + 'secret_realm2.png')
+        PAGUtils.click_gui2(img_daily_path + 'secret_realm1.png', img_daily_path + 'secret_realm2.png')
 
         # 兄贵健身房（普通玩家2次，月卡玩家3次）（可全部开放），兄贵的图有两种情况，一大一小
         self.fun(['dear_brother1.png', 'dear_brother2.png'], 2)
@@ -47,12 +48,12 @@ class SecretRealm:
         self.fun(['goddess.png'], 1)
 
         # 滚轮向下滚动n次
-        PyautoguiUtils.scroll(-1, 10)
+        PAGUtils.scroll(-1, 10)
 
         # 羁绊之地
         self.fun(['bond.png'], 1)
 
-        PyautoguiUtils.scroll(-1, 25)
+        PAGUtils.scroll(-1, 25)
 
         # 永恒的神殿
         self.fun(['temple.png'], 1)
@@ -60,7 +61,7 @@ class SecretRealm:
         # TODO 律定之途（可全部开放）
 
         # 返回主界面
-        PyautoguiUtils.click_gui(img_daily_path + 'back.png')
+        PAGUtils.click_gui(img_daily_path + 'back.png')
 
         print('<== 扫荡秘境结束')
 
