dc85ac80268846cbbd7d3e1da2bc756a5da13701
freeok <51998152+freeok@users.noreply.github.com>
2023-11-19 10:55:16 +0800

    :recycle: refactor: 重构 pyautogui_utils.click_gui()

--- src/util/window_utils.py
+++ src/util/window_utils.py
@@ -6,7 +6,7 @@ import pygetwindow as gw
 class WindowUtils:
 
     @staticmethod
-    def maximize(title, always=False):
+    def maximize(title, retry=False):
         while True:
             w_arr = gw.getWindowsWithTitle(title)
             if len(w_arr) > 0:
@@ -14,7 +14,8 @@ class WindowUtils:
                 w = w_arr[0]
                 # 窗口最大化
                 w.maximize()
-            if always:
+                break
+            if retry:
                 print('未检测到窗口：{}，1 秒后重试'.format(title))
                 time.sleep(1)
             else:
